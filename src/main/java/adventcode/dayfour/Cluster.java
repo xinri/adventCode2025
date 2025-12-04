c3acb7ecee6de10de9eef703e803e3f581d0db1e
xinri <hlay.fr@gmail.com>
2025-12-04 08:13:22 +0100

    day 4 with real example

---
+++ src/main/java/adventcode/dayfour/Cluster.java
@@ -0,0 +1,13 @@
+package adventcode.dayfour;
+
+public class Cluster {
+    private final String cluster;
+
+    public Cluster(String cluster) {
+        this.cluster = cluster;
+    }
+
+    public long findRollPaperAccessible() {
+        return 0;
+    }
+}
